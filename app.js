d1dfa1605ff90dc8b2d198e7d65e45cf3623e785
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-06 17:00:44 +0100

    Connect to MongoDB via .env value

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// setup mongoose
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+require("dotenv").config();
+const mongoDB = process.env.MONGO;
+
+// Connect to MongoDB
+main().catch((err) => console.error(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
