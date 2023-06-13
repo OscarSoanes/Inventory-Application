f585367b94b89e6bb687734c31344d893bb97ed7
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-13 12:08:04 +0100

    Added ability to post item form to database

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,7 @@
 const Item = require("../models/Item");
 const Category = require("../models/Category");
+
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all item
@@ -43,14 +45,65 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
   res.render("item_form", {
     title: "Create Item",
     all_categories: allCategories,
-    item: {}
+    item: {},
+    errors: [],
   })
 })
 
 // Handle item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create POST");
-})
+exports.item_create_post = [
+  // Validate and sanitize fields.
+
+  body("name", "Name must not be empty")
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+  body("desc", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+  body("number_in_stock", "Number in Stock must not be empty")
+    .trim()
+    .isNumeric()
+    .escape(),
+  body("price", "Price must not be empty")
+    .trim()
+    .isDecimal().withMessage("Price must be a decimal")
+    .escape(),
+  body("category", "Category must not be empty")
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+  
+    asyncHandler(async (req, res, next) => {
+      // Extract the errors from request
+      const errors = validationResult(req);
+
+      // Create a new item
+      const item = new Item({
+        name: req.body.name,
+        desc: req.body.desc,
+        number_in_stock: req.body.number_in_stock,
+        price: req.body.price,
+        category: req.body.category
+      })
+
+      if (!errors.isEmpty()) {
+        // Error
+        const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+        res.render("item_form", {
+          title: "Create Item",
+          all_categories: allCategories,
+          item: item,
+          errors: errors.array()
+        });
+      } else {
+        await item.save();
+        res.redirect(item.url);
+      }
+    })
+]
 
 // Display item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
